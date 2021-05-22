5ef6ee52ca7feab9abfc31ab6acf6a99e682d628
1InfinityDoesExist <patelavinash508@gmail.com>
2021-05-22 21:52:18 +0545

    exception added

---
+++ src/main/java/com/ksewa/springsecurity/exception/AlreadyExitException.java
@@ -0,0 +1,14 @@
+package com.ksewa.springsecurity.exception;
+
+public class AlreadyExitException extends BaseException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public AlreadyExitException(String msg) {
+		super(msg);
+	}
+
+}
